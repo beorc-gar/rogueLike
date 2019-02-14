c5097dea1d8a91604d4cdf54f23fac4f6d348a18
ᛒᚸ <9055837+beorc-gar@users.noreply.github.com>
2019-02-14 14:23:29 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,14 +1,5 @@
 /*******************************main.c*******************************************************
-Student Name: Bronson Graansma                Student Number: 0872249
 Date:         Monday, March 2nd, 2015         Course Name:    CIS*2500
-I have exclusive control over this submission via my password.
-By including this statement in this header comment, I certify that:
-     1) I have read and understood the University policy on academic integrity;
-     2) I have completed the Computing with Integrity Tutorial on Moodle; and
-     3) I have achieved at least 80% in the Computing with Integrity Self Test.
-I assert that this work is my own. I have appropriately acknowledged any and all material
-(data, images, ideas or words) that I have used, whether directly quoted or paraphrased.
-Furthermore, I certify that this assignment was prepared by me specifically for this course.
 *********************************************************************************************/
 
 #include "main.h"
